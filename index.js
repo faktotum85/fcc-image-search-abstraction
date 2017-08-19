8df9ca0170aac280aba1a9d8c404d69e64c3a654
Simon Weller <s.meierbeck@gmail.com>
2017-08-19 12:05:48 +0200

    added offsets

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 8080;
 
 app.get('/api/imagesearch/:searchTerm', (req, res) => {
   const searchTerm = req.params.searchTerm;
-  request.get(`https://www.googleapis.com/customsearch/v1?cx=000492143642458176317:8sltgvz-ir0&q=${searchTerm}&key=AIzaSyCoycQQfVqnwrpPE7KMwGxoDQloYuToRx8&searchType=image`, (err, response, body) => {
+  const offset = req.query.offset;
+  let url = `https://www.googleapis.com/customsearch/v1?cx=000492143642458176317:8sltgvz-ir0&q=${searchTerm}&key=AIzaSyCoycQQfVqnwrpPE7KMwGxoDQloYuToRx8&searchType=image`;
+  if (offset) {
+    url += '&start=' + (+offset + 1);
+  }
+  request.get(url, (err, response, body) => {
     if (!err && response.statusCode === 200) {
       const items = JSON.parse(body).items;
       res.json(items.map(item => {
